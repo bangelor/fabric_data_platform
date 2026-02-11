32888b1c97c195dbd305bf07cdff5f242270c30d
Lorenz Bangerter <48515134+bangelor@users.noreply.github.com>
2026-02-11 17:41:51 +0100

    fixed variable group bug (#28)
    * fixed variable group bug
    
    * fix

--- infra/modules/core-workspace/main.tf
+++ infra/modules/core-workspace/main.tf
@@ -50,7 +50,8 @@ resource "fabric_variable_library" "deployment" {
 
   definition = {
     "settings.json" = {
-      source = "${path.module}/definitions/settings.json"
+      source          = "${path.module}/definitions/settings.json"
+      processing_mode = "None"
     }
     "variables.json" = {
       source = "${path.module}/definitions/variables.json"
